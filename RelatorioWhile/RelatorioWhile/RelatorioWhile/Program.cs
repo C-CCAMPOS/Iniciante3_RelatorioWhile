b90e6104654d722a1fe28b57e778cce1cad6314f
CleberCampos2011 <clebercampos2011@hotmail.com>
2022-11-19 22:21:09 -0300

    Acabamento final Id1

--- RelatorioWhile/RelatorioWhile/RelatorioWhile/Program.cs
+++ RelatorioWhile/RelatorioWhile/RelatorioWhile/Program.cs
@@ -22,57 +22,63 @@ namespace relatorioWhile
             double menor = 0;
             double media = 0;
             double mora = 0;
-                       
+
 
             while (id < 5)
-            {                
-                Console.WriteLine("\n\n-----------------xxxxxxxxxxxxxxx------------------");
-                Console.WriteLine("1- Maior e Menor valor da lista.");
-                Console.WriteLine("2- Ordenar a lista de forma crescente.");
-                Console.WriteLine("3- Ordenar a lista de forma decrescente.");
-                Console.WriteLine("4- Calculo da Média + Mora.");
-                Console.WriteLine("\n\n5- Caso deseja finalizar.");
+            {
+                Console.WriteLine("\n\n-----------------xxxxxxxxxxxxxxx------------------");               
+                Console.WriteLine("|1| Maior e Menor valor da lista.");
+                Console.WriteLine("|2| Ordenar a lista de forma crescente.");
+                Console.WriteLine("|3| Ordenar a lista de forma decrescente.");
+                Console.WriteLine("|4| Calculo da Média + Mora.");
+                Console.WriteLine("\n|5| Caso deseja finalizar.");
                 Console.WriteLine("**************************************************");
 
-                Console.Write("\n\nDigite a opção desejada: ");
+                Console.Write("\nDigite a opção desejada: ");
                 id = Convert.ToInt32(Console.ReadLine());
-                Console.Write("**************************************************");
-                
-                Console.Write("\n\n");
+                Console.WriteLine("**************************************************");
 
+                Console.Write("\nInforme a quantidade de operações que deseja realizar: ");
+                qtd = Convert.ToInt32(Console.ReadLine());
+                                 
+                Console.Write("\nDigite o valor da operação " + 1 + ": ");
+                valor = Convert.ToDouble(Console.ReadLine());
+                maior = valor;
+                menor = valor;
+                i = 1;
+
+                for (i = 2; i <= qtd; i++)
+                {
+                    Console.Write("Digite o valor da operação " + i + ": ");
+                    valor = Convert.ToDouble(Console.ReadLine());
+                }
+
+                            
+                                                
                 if (id == 1)
                 {
-                    Console.Write("\nDigite a quantidade de operações que deseja realizar: ");
-                    qtd = Convert.ToInt32(Console.ReadLine());                   
 
-                    Console.Write("\n\nDigite o valor da operação " + 1 + ": ");
-                    valor = Convert.ToDouble(Console.ReadLine());
-                    maior = valor;
-                    menor = valor;
-                    i = 1;
-                  
-                    for (i = 2; i <= qtd; i++)
+
+                    if (valor < menor)
                     {
-                        Console.Write("Digite o valor da operação " + i + ": ");
-                        valor = Convert.ToDouble(Console.ReadLine());            
-
-                        if (valor < menor)
-                        {
-                            menor = valor;
-                        }
-
-                        if (valor > maior)
-                        {
-                            maior = valor;
-                        }                        
+                        menor = valor;
                     }
-                    Console.Write("**************************************************");
-                    Console.WriteLine("\n\nMAIOR VALOR: " + maior);
-                    Console.WriteLine("*****************");
-                    Console.WriteLine("MENOR VALOR: " + menor);
-                    Console.WriteLine("*****************");
-                    Console.ReadKey();
-                } 
+
+                    if (valor > maior)
+                    {
+                        maior = valor;
+                    }
+                }
+
+                Console.WriteLine("\n-----------------xxxxxxxxxxxxxxx------------------");
+                Console.WriteLine("MAIOR VALOR |" + maior + "|");
+                Console.WriteLine("MENOR VALOR |" + menor + "|");
+                Console.WriteLine("-----------------xxxxxxxxxxxxxxx------------------");
+                Console.Write("\n\n");                
+                Console.ReadKey();
+                Console.Clear();
+
+
             }
         }
     }
